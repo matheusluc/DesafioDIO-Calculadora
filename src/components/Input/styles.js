a681f879ee6c311663d690a817f18b57da3d63a0
Matheus Lucena <matheus.supervisor@gmail.com>
2023-03-01 09:45:38 -0300

    finalizado o desafio de codigo da calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flez;
+        flex-direction: column:
+        align-items: flex-end;
+        padding: 0 10px;
+        color: #FFFFFF;
+        
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
